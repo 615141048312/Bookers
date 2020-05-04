d73cb24362e599f0d6d1ddb22541ccbb4f76caca
花田　結衣 <yui@hanadayuinoMBP.lan>
2020-05-04 21:06:06 +0900

    commit 3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,7 @@ class BooksController < ApplicationController
     book = Book.new(book_params)
     book.user_id = current_user.id
     book.save
-    redirect_to book_path
+    redirect_to book_path(book.id)
   end
 
   def index
@@ -15,24 +15,28 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     #user.id == @book.user_id
-  	@user = User.find(params[:book.user_id])
+  	@user = User.find_by(id: @book.user_id)
     @new_book = Book.new
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
   end
 
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
     user = current_user
-    redirect_to user_path(user.id)
+    redirect_to book_path
   end
 
-  def update
-  end
-
-  protected
+  private
 
   def book_params
     params.require(:book).permit(:title, :opinion, :user_id)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+
+  def root
+  end
+
+  def about
+  end
+
   def index
   	@user = current_user
     @new_book = Book.new
@@ -12,8 +19,18 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 end
